e8349aeb091bd04435f9fa54df704f565458414a
Will Decker <will.decker@gatech.edu>
2024-10-08 19:27:14 -0400

    fixed data install bug

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="dbword",
-    version="1.0.0",
+    version="1.0.1",
     author="Will Decker",
     author_email="will.decker@gatech.edu",
     description="Accessing word statistics databases",
     url="https://github.com/GT-LIT-Lab/dbword",
     packages=find_packages(),
+    package_data={
+        'dbword': ['data/subtlex-us.pkl'],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
