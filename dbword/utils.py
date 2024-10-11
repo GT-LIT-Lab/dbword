28e96554cdad867cf3b37681bd214d386e581f47
Will Decker <will.decker@gatech.edu>
2024-10-11 08:24:38 -0400

    refactor for better usage

--- dbword/utils.py
+++ dbword/utils.py
@@ -1,7 +1,8 @@
 import requests
 import zipfile
-from dbword import SUBTLEX_PATH
+from .config import SUBTLEX_PATH, KUPERMAN_PATH
 import pickle
+import pandas as pd
 import os
 
 def download(db:str, verbose:bool=True):
@@ -17,32 +18,62 @@ def download(db:str, verbose:bool=True):
     if db == 'subtlex':
         _subtlex_us(verbose)
         _unzip(db, verbose)
-    
-    def _subtlex_us(verbose:bool):
-        url = "https://www.ugent.be/pp/experimentele-psychologie/en/research/documents/subtlexus/subtlexus2.zip"
-        file_path = "data/subtlexus2.zip"
-        response = requests.get(url)
+        _ = convert_to_pickle(path=os.path.join(SUBTLEX_PATH, "/data/SUBTLEXus74286wordstextversion.txt"),
+                          fname="subtlex-us", verbose=verbose)
 
-        # Write the content to a file
-        with open(file_path, "wb") as file:
-            file.write(response.content)
+    if db == 'kuperman':
+        _kuperman(verbose)
+        _ = convert_to_pickle(path=os.path.join(KUPERMAN_PATH, "/data/kuperman.txt"),
+                          fname='kuperman', verbose=verbose)
 
-        if verbose:
-            print(f"File downloaded and saved as {file_path}")
+def _subtlex_us(verbose:bool):
+    url = "https://www.ugent.be/pp/experimentele-psychologie/en/research/documents/subtlexus/subtlexus2.zip"
+    file_path = os.path.join(SUBTLEX_PATH, "/data/subtlexus2.zip")
+    response = requests.get(url)
+    response.raise_for_status()
 
-    def _unzip(db:str, verbose:bool):
+    # Write the content to a file
+    with open(file_path, "wb") as file:
+        file.write(response.content)
 
-        if db == "subtlex":
-            file = "data/subtlexus2.zip"
+    if verbose:
+        print(f"File downloaded and saved as {file_path}")
 
-        with zipfile.ZipFile(file, 'r') as zip_ref:
-            zip_ref.extractall(".")
+def _unzip(db:str, verbose:bool):
 
-        if verbose:
-            print(f"File unzipped and saved as data/SUBTLEXus74286wordstextversion.txt")
+    if db == "subtlex":
+        file = os.path.join(SUBTLEX_PATH, "/data/subtlexus2.zip")
+
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(".")
+
+    if verbose:
+        print(f"File downloaded, unzipped and saved as data/SUBTLEXus74286wordstextversion.txt")
+
+def _kuperman(verbose:bool):
+    url = "https://static-content.springer.com/esm/art%3A10.3758%2Fs13428-013-0348-8/MediaObjects/13428_2013_348_MOESM1_ESM.xlsx"
+    file_path = os.path.join(KUPERMAN_PATH, "/data/13428_2013_348_MOESM1_ESM.xlsx")
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+
+    new_file_path = os.path.join(KUPERMAN_PATH, "/data/kuperman.xlsx")
+
+    # go ahead and rename file here
+    os.rename(file_path, new_file_path)
+
+    # convert to txt
+    df = pd.read_excel(new_file_path)
+    txt_file_path = os.path.join(KUPERMAN_PATH, "/data/kuperman.txt")
+    df.to_csv(txt_file_path, sep='\t', index=False)
+
+    if verbose:
+        print(f"File downloaded and saved as {txt_file_path}")       
 
 def convert_to_pickle(path:str, fname:str, verbose:bool=False):
-    """Convert downloaded Subtlex file into pickle
+    """Convert database into pickle
     
     Parameters
     ----------
@@ -54,7 +85,7 @@ def convert_to_pickle(path:str, fname:str, verbose:bool=False):
         
     Returns
     -------
-    subtlex: dict
+    database: dict
         Dictionary of Subtlex database
     """
 
@@ -64,8 +95,8 @@ def convert_to_pickle(path:str, fname:str, verbose:bool=False):
     data = {} 
 
     with open(path, 'r') as file:
-       
-        # this will skip the headers
+    
+        # skip the headers
         header = file.readline().strip().split('\t')
         
         # create the dict
@@ -79,5 +110,4 @@ def convert_to_pickle(path:str, fname:str, verbose:bool=False):
         pickle.dump(data, pkl_file)
 
     if verbose:
-        print(f"Data has been saved as '{fname}.pkl'")
-
+        print(f"Database converted to pickle: '{fname}.pkl'")
