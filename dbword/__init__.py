4d89f661c9ffe7a8aa96a558bf459659f246579b
Will Decker <will.decker@gatech.edu>
2024-12-01 21:30:55 -0500

    remove redundancies

--- dbword/__init__.py
+++ dbword/__init__.py
@@ -10,15 +10,9 @@ def check_subtlex() -> bool:
     """Check if database exists in root""" 
     return os.path.isfile(SUBTLEX_PATH)
     
-if not check_subtlex():
-    warnings.warn(f"Subtlex database not found \n Try running dbword.utils.download('subtlex')", UserWarning)
-
 def check_kuperman() -> bool:
     """Check if database exists in root""" 
     return os.path.isfile(KUPERMAN_PATH)
-
-if not check_kuperman():
-    warnings.warn(f"Kuperman database not found \n Try running dbword.utils.download('kuperman')", UserWarning)
 
 def __getattr__(name):
     if name == 'Kuperman':
