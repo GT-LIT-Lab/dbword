44b9eed70279ca54d9efa97313cbb18cfe2b7275
Will <deckerwill7@gmail.com>
2025-03-17 21:32:13 -0400

    `v3.3.0`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dbword",
-    version="2.3.1",
+    version="3.0.0",
     author="Will Decker",
     author_email="will.decker@gatech.edu",
     description="Accessing word statistics databases",
